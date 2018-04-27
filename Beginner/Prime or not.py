b0ebc2a7855f3571b745c61b3a3aed20d3385abf
vaantablaack <37567581+vaantablaack@users.noreply.github.com>
2018-04-27 15:49:50 +0530

    Create Prime or not.py

---
+++ Beginner/Prime or not.py
@@ -0,0 +1,8 @@
+num=input('Enter the number')
+for i in range(2,num):
+  if (num==73):
+    print('Optimus Prime _/\_')
+  elif(num%i==0):
+    print('Nah, Not a Prime')
+  else:
+    print('You a Prime!')
